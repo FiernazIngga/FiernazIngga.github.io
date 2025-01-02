9c4ea63e64b52895913ad496ae741fefc795895b
FiernazIngga <inggapratama86@gmail.com>
2025-01-02 21:35:26 +0700

    first commit

---
+++ public/tailwind.config.js
@@ -0,0 +1,29 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*/.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: '#effeeb',
+        paragraph: '#045103',
+        headline: '#094067',
+        buttonText: '#fffffe',
+        button: '#3da9fc',
+        stroke: '#094067',
+        main: '#fffffe',
+        highlight: '#3da9fc',
+        secondary: '#90b4ce',
+        tertiary: '#ef4565'
+      },
+      height: {
+        'custom-small': '53.5rem'
+      },
+      width: {
+        'custom-small': '30rem',
+        'custom-medium': '50%',
+      }
+    },
+  },
+  plugins: [],
+}
